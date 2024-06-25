93db1292075f6e13e03e6c38c5bf544b6bcca35f
minimalparts <aurelie.herbelot@cantab.net>
2024-06-25 16:52:14 +0200

    Added VG extraction scripts.

--- README.md
+++ README.md
@@ -6,4 +6,17 @@ The GPT training code in this repository is mostly taken from the excellent [nan
 
 ## Data
 
+## Activities
 
+Your system will train on different types of activities, linked to different input and output formats. At present time, activities can be:
+
+* observation
+* reading
+* skill-training
+* hearing conversations
+* having conversations
+* experimenting
+* thinking
+* dreaming
+
+Different types of data will be suited to one or more of these activities. For instance, the Visual Genome data is good for *observing* and *skill-training*.
--- data/visual_genome/extract.py
+++ data/visual_genome/extract.py
@@ -4,10 +4,12 @@ import requests
 import zipfile
 from tqdm import tqdm
 from os.path import join
+from pathlib import Path
 
 host_url = "https://homes.cs.washington.edu/~ranjay/visualgenome/data/dataset/"
 
-def download_qa(fpath):
+def download_vg(fpath):
+    print(f">> DATA: VISUAL GENOME DOWNLOADING: {fpath}")
     response = requests.get(join(host_url, fpath), stream=True)
     total_size = int(response.headers.get("content-length", 0))
     block_size = 1024
@@ -16,21 +18,68 @@ def download_qa(fpath):
             for chunk in response.iter_content(block_size):
                 progress_bar.update(len(chunk))
                 f.write(chunk)
-    print(fpath,"successfully downloaded")
-
-fpath = 'question_answers.json.zip'
-download_qa(fpath)
-
-with zipfile.ZipFile(fpath) as zf:
-    with io.TextIOWrapper(zf.open(fpath.replace('.zip','')), encoding="utf-8") as f:
-    #with open(fpath, "r", encoding="utf-8") as fi:
-     content = json.load(f)
+    print(f">> DATA: VISUAL GENOME DOWNLOADING: {fpath} successfully downloaded.\n")
 
 
-for item in content:
-    qas = item['qas']
-    for qa in qas:
-        question = qa['question']
-        answer = qa['answer']
-        if len(answer.split()) > 3:
-            print(f"<u speaker=HUM>{question}</u>\n<u speaker=BOT>{answer}</u>\n")
+def extract_rd_for_training(fpath, activity=None):
+    if activity not in ['obs']:
+        print(f">> DATA: VISUAL GENOME EXTRACTION: ERROR: activity type {activity} not supported.")
+        return 0
+
+    print(f">> DATA: VISUAL GENOME EXTRACTING: {fpath} for training ({activity})")
+    Path(activity).mkdir(parents=True, exist_ok=True)
+
+    with zipfile.ZipFile(fpath) as zf:
+        with io.TextIOWrapper(zf.open(fpath.replace('.zip','')), encoding="utf-8") as f:
+         content = json.load(f)
+
+    with open(join(activity,fpath.replace('.zip','.'+activity)), 'w', encoding="utf-8") as f:
+        c = 0
+        for img in content:
+            idx = int(img['id'])
+            if idx != c:
+                if c != 0:
+                    f.write(f"</a>\n")
+                c = idx
+                f.write(f"<a type=OBS>\n")
+            for region in img['regions']:
+                phrase = region['phrase'].rstrip('\n')
+                f.write(f"{phrase}\n")
+        f.write(f"</a>\n")
+    print(f">> DATA: VISUAL GENOME EXTRACTING: extracted {c} situations.\n")
+
+
+def extract_qa_for_training(fpath, activity=None):
+    if activity not in ['skt']:
+        print(f">> DATA: VISUAL GENOME EXTRACTION: ERROR: activity type {activity} not supported.")
+        return 0
+
+    print(f">> DATA: VISUAL GENOME EXTRACTING: {fpath} for training ({activity})")
+    d = 'skill_training'
+    Path(activity).mkdir(parents=True, exist_ok=True)
+
+    with zipfile.ZipFile(fpath) as zf:
+        with io.TextIOWrapper(zf.open(fpath.replace('.zip','')), encoding="utf-8") as f:
+         content = json.load(f)
+
+    with open(join(activity,fpath.replace('.zip','.'+activity)), 'w', encoding="utf-8") as f:
+        c = 0
+        for item in content:
+            qas = item['qas']
+            for qa in qas:
+                question = qa['question']
+                answer = qa['answer']
+                if len(answer.split()) > 3:
+                    if activity == 'skt':
+                        f.write(f"<a type=SKT>\n")
+                        f.write(f"<u speaker=HUM>{question}</u>\n<u speaker=BOT>{answer}</u>\n")
+                        f.write(f"</a>\n")
+                    c+=1
+    print(f">> DATA: VISUAL GENOME EXTRACTING: extracted {c} QA pairs.\n")
+
+
+qapath = 'question_answers.json.zip'
+rdpath = 'region_descriptions.json.zip'
+#download_vg(rdpath)
+#extract_qa_for_training(qapath,'skt')
+extract_rd_for_training(rdpath,'obs')
